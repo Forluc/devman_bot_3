43764e831fd1971f4e32b29fbcb7c8075ae0aa39
Dmitrii Masliaev <m6ytcbi@gmail.com>
2024-01-04 21:18:57 +0400

    If intent.is_fallback, the bot will Vk silent

--- vk_bot.py
+++ vk_bot.py
@@ -1,30 +1,56 @@
+import logging
 import random
 
 import vk_api as vk
 from environs import Env
+from google.cloud import dialogflow
 from vk_api.longpoll import VkEventType, VkLongPoll
 
-from tg_bot import get_detect_intent_texts, get_google_credentials
+from tg_bot import get_google_credentials
 
 env = Env()
 env.read_env()
 
 vk_api_key = env.str('VK_API_KEY')
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__file__)
 
-def sending_dialogflow_messages(event, vk_api):
+
+def get_detect_intent_texts(project_id: str, session_id: str, text: str, language_code: str) -> str:
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    logger.info(f'Session path: {session}')
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(request={'session': session, 'query_input': query_input})
+
+    logger.info(f'Query text: {response.query_result.query_text}')
+    logger.info(
+        f'Detected intent: {response.query_result.intent.display_name}'
+        f'(Confidence: {response.query_result.intent_detection_confidence})'
+    )
+    logger.info(f'Fulfillment text: {response.query_result.fulfillment_text}')
+
+    if not response.query_result.intent.is_fallback:
+        return response.query_result.fulfillment_text
+
+
+def sending_dialogflow_messages(event, vk_api) -> None:
     reply_text = get_detect_intent_texts(
         project_id=get_google_credentials()['quota_project_id'],
         session_id=event.user_id,
         text=event.text,
         language_code='ru'
     )
-
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=reply_text,
-        random_id=random.randint(1, 1000)
-    )
+    if reply_text:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=reply_text,
+            random_id=random.randint(1, 1000)
+        )
 
 
 if __name__ == "__main__":
